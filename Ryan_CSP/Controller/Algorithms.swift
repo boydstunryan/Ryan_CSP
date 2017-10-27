3c3513f2d4065f94fe5f6a02bf8103f39ddcccdc
boydstunryan <boydstunryan@gmail.com>
2017-10-27 08:39:49 -0600

    Added the framework

---
+++ Ryan_CSP/Controller/Algorithms.swift
@@ -0,0 +1,9 @@
+//
+//  Algorithms.swift
+//  Ryan_CSP
+//
+//  Created by Boydstun, Ryan on 10/26/17.
+//  Copyright © 2017 Boydstun, Ryan. All rights reserved.
+//
+
+import Foundation
---
+++ Ryan_CSP/Controller/Internet.swift
@@ -0,0 +1,9 @@
+//
+//  Internet.swift
+//  Ryan_CSP
+//
+//  Created by Boydstun, Ryan on 10/26/17.
+//  Copyright © 2017 Boydstun, Ryan. All rights reserved.
+//
+
+import Foundation
